eb4a92eb0399c281c41ae766c7afc13914d140e8
suranimayur <surani.mayur@gmail.com>
2025-01-08 22:44:25 +0530

    Unit test case updated

---
+++ tests/test_operation.py
@@ -0,0 +1,10 @@
+from src.math_opertion import add,subtract,subtract
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-6, 8) == 3
+    
+def test_subtract():
+    assert subtract(1, 2) == -1
+    assert subtract(-6, 8) == -12
+
